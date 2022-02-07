9f29d595a97c96e7c9ab4eff1070300103d69b6c
Conghung43 <35483984+Conghung43@users.noreply.github.com>
2022-02-07 15:00:24 +0800

    create record video

---
+++ record_video.py
@@ -0,0 +1,11 @@
+    # video_export_1 = cv2.VideoWriter("video_log/video_export_1_{}.mp4".format(current_time), 0x7634706d, 30, (1920,1080))
+    # video_export_2 = cv2.VideoWriter("video_log/video_export_2_{}.mp4".format(current_time), 0x7634706d, 30, (1920,1080))
+    
+                        # if index == 0:
+                    #     video_export_1.write(image)
+                    # else:
+                    #     video_export_2.write(image)
+                    # cv2.imshow(str(name_list[index]), image)
+              
+    # video_export_1.release()
+    # video_export_2.release()
