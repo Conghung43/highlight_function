6d4ca71218f02a43c62de30812f08b6af00c84d1
Conghung43 <35483984+Conghung43@users.noreply.github.com>
2022-03-20 13:21:59 +0800

    Update line polygon
    add draw polygon

--- visualization.py
+++ visualization.py
@@ -9,3 +9,11 @@ def show_image(tab_name, image, is_iteration):
         cv2.waitKey(1)
     else:
         cv2.waitKey(0)
+
+        
+# Draw line from polygon
+            #     for index, point in enumerate(polygons):
+            #         if index >= len(polygons) - 1:
+            #             vis_frame = cv2.line(vis_frame,tuple(point),tuple(polygons[0]),(0, 255, 0),1)
+            #         else:
+            #             vis_frame = cv2.line(vis_frame,tuple(point),tuple(polygons[index + 1]),(0, 255, 0),1)
