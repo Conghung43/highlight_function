e8cb1bbecbd00154414e05cca69553e9a15c49d7
Conghung43 <35483984+Conghung43@users.noreply.github.com>
2022-03-26 13:32:40 +0800

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -11,3 +11,23 @@ plt.scatter(current_ps_pixel_data[:, 0],
 # plt.show()
 plt.pause(1)
 plt.clf()
+
+
+ax = plt.axes(projection ="3d")
+
+image = cv2.imread('dummy_data/objects_collection.jpg')
+f =  open('dummy_data/objects_collection.npy', 'rb')
+objects_collection = np.load(f, allow_pickle=True)
+objects_collection = np.array([objects_collection[index] for index in range(0, len(objects_collection),50)])
+x,y,z = [objects_collection[:,0], 
+                    objects_collection[:,1], 
+                    objects_collection[:,2]]
+print(len(objects_collection))
+sctt = ax.scatter3D(x,y,z,
+                    alpha = 0.8,
+                    c = (x + y + z),
+                    )
+ax.set_xlim([0,255])
+ax.set_ylim([0,255])
+ax.set_zlim([0,255])
+plt.show()
