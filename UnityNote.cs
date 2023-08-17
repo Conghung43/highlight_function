d0f3dcd1ce8998f5f2eb51711db4e0ae05cf0a68
Conghung43 <35483984+Conghung43@users.noreply.github.com>
2023-08-17 08:51:48 +0800

    Create UnityNote.cs

---
+++ UnityNote.cs
@@ -0,0 +1,5 @@
+
+1. Execute time:
+//Stopwatch stopwatch = new Stopwatch(); stopwatch.Start();
+//stopwatch.Stop(); long elMs = stopwatch.ElapsedMilliseconds; UnityEngine.Debug.Log("NormalizeImage time:" + elMs + "ms");//stopwatch.Reset(); stopwatch.Start();
+
